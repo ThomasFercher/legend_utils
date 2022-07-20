1d003306c10495d4c416d189174afba273708b6d
Thomas Fercher <thomas.fercher@outlook.com>
2022-07-20 23:10:08 +0200

    Added Path Url Strategy

--- lib/legend_utils.dart
+++ lib/legend_utils.dart
@@ -1,5 +1,7 @@
 library legend_utils;
 
 export './extensions/extensions.dart';
+export './extensions/colors.dart';
 export './functions/functions.dart';
 export './widgets/restart.dart';
+export './urlstrategy/url_strategy.dart';
